074b093930f35d3b360f2e7be23b5513e1ef313a
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-09-19 16:12:57 -0700

    Update processListenerRatings.R

---
+++ processListenerRatings.R
@@ -0,0 +1,10 @@
+# Plan:
+# Go through the relabelCHN assignments and identify the relabelings that are
+# finished and can be kept and discard those that ar in-progress
+# While at it, create a master list of files to be included, together with some
+# metadata for each
+# Get the modal rating for each LENA segment
+# Perhaps do this using only first pass or using 2nd pass if available
+# Computer inter-rater and intra-rater reliability
+# Get clean lists of the subset of clips that are clean as defined by Pagliarini
+# et al. (2022)
