c49f32ddfc279e03fe9f4ea840ed47fbbe410710
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-10-03 15:17:52 -0700

    Bug fix and new code
    Fixed a bug in the original way of determining clean clips and added functionality to do the cleaning only on listeners with high inter-rater reliability compared to me.

--- 4_identifyReliableListeners.R
+++ 4_identifyReliableListeners.R
@@ -1,17 +1,4 @@
-# Some files have no clean infant clips based on a modal rating == 1 criterion.
-# Let's check interrater reliability and disregard raters with poor agreement,
-# and see if that leads to more clips being identified as clean.
-# Maybe some raters were much more conservative than others in giving a 1 rating.
-# I will start by checking agreement with myself, listener id lplf, on the files
-# I relabeled. If there are some listeners who never did files that I did, I can
-# compare them to the listener who was most reliabile agains my own ratings
-# (we'll want to consider both Cohen's kappa and number of files we both did;
-# if someone has high cohen's kappa but that's only based on a small number of
-# recordings we might not want to have them be the stand-in for me in judging
-# other listeners. But let's see what the situation is.).
-
-# Check which assignments I completed, and which listeners did or did not have
-# overlap with mine. Can get this info. from completed_relabel_file_info.csv
+# Get interrater reliability in comparison to the primary (reference) listener
 
 library(irr)
 
@@ -21,11 +8,13 @@ setwd("~/Documents/GitHub/IVFCR-UMAP/cleaning_metadata")
 completed_relabels <- read.csv("completed_relabel_file_info.csv")
 listeners_cleaning_data <- read.csv("listeners_cleaning_data.csv")
 
+# Check which assignments I completed, and which listeners did or did not have
+# overlap with mine.
 ref_completed <- subset(completed_relabels,listenerID==ref_listener)
 other_listeners <- subset(listeners_cleaning_data,listenerID!=ref_listener)$listenerID
 
-w_kappas_df <- data.frame(recording <- ref_completed$wav_filename)
-u_kappas_df <- data.frame(recording <- ref_completed$wav_filename)
+w_kappas_df <- data.frame(recording = ref_completed$wav_filename)
+u_kappas_df <- data.frame(recording = ref_completed$wav_filename)
 
 for (i in 1:nrow(ref_completed)){
   
---
+++ 5_identifyBetterCleanSegments.r
@@ -0,0 +1,38 @@
+# Let's include only listeners with weighted kappa >= .8
+
+library(dplyr)
+
+setwd("~/Documents/GitHub/IVFCR-UMAP/cleaning_metadata")
+w_kappas <- read.csv("weighted_kappas.csv")
+
+# Get the average kappa for each listener
+w_kappas$recording <- NULL
+w_kappas$X <- NULL
+avg_kappas <- colMeans(w_kappas, na.rm = FALSE)
+best_listeners <- names(which(avg_kappas>=.8))
+
+clip_files <- list.files(pattern = "clip_labels*")
+for (f in clip_files){
+  
+  clip_labels <- read.csv(f)
+  best_clip_labels <- clip_labels %>% select(starts_with(best_listeners))
+  modal_prom <- numeric()
+  cleanB <- logical()
+  
+  # add a modal value column and a clean (TRUE/FALSE) column
+  for (c in 1:nrow(best_clip_labels)){
+    freqs <- table(as.numeric(best_clip_labels[c,]))
+    modal_prom[c] <- as.numeric(names(which.max(freqs)))
+    if (modal_prom[c]==1){
+      cleanB[c] <- TRUE
+    } else{
+      cleanB[c] <- FALSE
+    }
+  }
+  
+  best_clip_labels$modal_prominence <- modal_prom
+  best_clip_labels$clean <- cleanB
+  
+  write.csv(best_clip_labels,paste("best_",f,sep=""))
+  
+}
