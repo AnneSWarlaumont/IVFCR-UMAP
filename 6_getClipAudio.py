588048a65f823c4c0bf6cc04b81d1cc91c295c06
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-10-03 16:43:50 -0700

    Working on extracting audio clips

---
+++ 6_getClipAudio.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import pyglet, scipy.io.wavfile, numpy, os
+
+SJV_dir = "~/Library/CloudStorage/Box-Box/SanJoaquinValleyCorpus_Public/"
+os.chdir(os.path.expanduser("~/Documents/GitHub/IVFCR-UMAP/cleaning_metadata/"))
+
