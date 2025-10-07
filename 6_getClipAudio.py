d2f84a5b7508b9ebee9cf6d3b5f8862c043a1876
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-10-07 12:46:53 -0700

    Got clean clips' wav files.

--- 6_getClipAudio.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import pyglet, scipy.io.wavfile, numpy, os
-
-SJV_dir = "~/Library/CloudStorage/Box-Box/SanJoaquinValleyCorpus_Public/"
-os.chdir(os.path.expanduser("~/Documents/GitHub/IVFCR-UMAP/cleaning_metadata/"))
-
