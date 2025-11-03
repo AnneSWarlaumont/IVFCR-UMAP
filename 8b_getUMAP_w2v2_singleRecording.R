2ad4f2e157cc4cc83e7d34fc0009f4349aaa966c
Anne S. Warlaumont <warlaumont@ucla.edu>
2025-11-03 12:22:37 -0800

    updated videos

--- 8b_getUMAP_w2v2_singleRecording.R
+++ 8b_getUMAP_w2v2_singleRecording.R
@@ -35,7 +35,7 @@ for (l in 1:12){
   start_times <- sub("^(?:[^_]*_){2}([0-9.]+).*","\\1",emb_data_scaled$filename)
   end_times <- sub("^(?:[^_]*_){3}([0-9.]+).wav","\\1",emb_data_scaled$filename)
   
-  e_u_df <- data.frame(x=emb_umap_proj[,1],y=emb_umap$layout[,2],time=as.numeric(start_times),endtime=as.numeric(end_times),wavF=emb_data_scaled$filename)
+  e_u_df <- data.frame(x=emb_umap$layout[,1],y=emb_umap$layout[,2],time=as.numeric(start_times),endtime=as.numeric(end_times),wavF=emb_data_scaled$filename)
   e_p_df <- data.frame(x=emb_pca$x[,1],y=emb_pca$x[,2],time=as.numeric(start_times),endtime=as.numeric(end_times),wavF=emb_data_scaled$filename)
   
   e_baseplot <- ggplot(e_u_df, aes(x,y,color=time)) +
@@ -70,78 +70,102 @@ for (l in 1:12){
     scale_color_viridis_c(option = "cividis") +
     theme_minimal()
   ggsave(paste(u_pngDir,"umap_baseplot.png",sep=""),width=5,height=4,dpi=300)
-  # p_baseplot <- ggplot(b_p_data, aes(x,y,color=time)) +
-  #   geom_point(size = 1, shape = 1) +
-  #   scale_color_viridis_c(option = "cividis") +
-  #   theme_minimal()
-  # ggsave(paste(p_pngDir,"pca_baseplot.png",sep=""),width=5,height=4,dpi=300)
+  p_baseplot <- ggplot(b_p_data, aes(x,y,color=time)) +
+    geom_point(size = 1, shape = 1) +
+    scale_color_viridis_c(option = "cividis") +
+    theme_minimal()
+  ggsave(paste(p_pngDir,"pca_baseplot.png",sep=""),width=5,height=4,dpi=300)
   
   # create the plots with a large dot on the current vocalization and save pngs
   for (i in 1:nrow(b_u_data)){
     u_i_plot <- u_baseplot +
       geom_point(data = b_u_data[i,], aes(x,y,color=time), size = 5, shape = 19)
     ggsave(paste(u_pngDir,"voc",i,"_u_plot.png",sep=""),width=5,height=4,dpi=300)
-    # p_i_plot <- p_baseplot +
-    #   geom_point(data = b_p_data[i,], aes(x,y,color=time), size = 5, shape = 19)
-    # ggsave(paste(p_pngDir,"voc",i,"_p_plot.png",sep=""),width=5,height=4,dpi=300)
+    p_i_plot <- p_baseplot +
+      geom_point(data = b_p_data[i,], aes(x,y,color=time), size = 5, shape = 19)
+    ggsave(paste(p_pngDir,"voc",i,"_p_plot.png",sep=""),width=5,height=4,dpi=300)
   }
   
   wavParentDir <- "cleaning_metadata/"
+  comb_aud_file <- paste(umapDir,baby,"_combined.wav",sep="")
   
   fps <- 30
   prev_end <- b_u_data$time[1]
-  all_frames <- c()
-  all_audio <- list()
+  all_frames_umap <- c()
+  all_frames_pca <- c()
+  if (l==1){
+    all_audio <- list() 
+  }
   
   for (i in 1:nrow(b_u_data)){
     
     # set the pause time before dot and audio should appear
     pause_sec <- b_u_data$time[i]-prev_end
     
-    u_plot_file <- paste(u_pngDir,"voc",i,"_u_plot.png",sep="")
+    if (l==1){
+      # read in this voc's wav file
+      iWavF <- b_u_data$wavF[i]
+      rec <- sub("^((?:[^_]*_){2}).*","\\1",iWavF)
+      iWavFP <- paste(wavParentDir,"best_clip_labels_",rec,"wavFiles/",iWavF,sep="")
+      wav <- readWave(iWavFP)
+    }
     
-    # read in this voc's wav file
-    iWavF <- b_u_data$wavF[i]
-    rec <- sub("^((?:[^_]*_){2}).*","\\1",iWavF)
-    iWavFP <- paste(wavParentDir,"best_clip_labels_",rec,"wavFiles/",iWavF,sep="")
-    wav <- readWave(iWavFP)
-    
-    # create the silent pause on the baseplot image
+    # create the silent pause on the baseplot image and, if applicable, in the audio file
     if (pause_sec>0){
       silent_seconds <- round(log10(pause_sec+1)*fps)/fps
-      all_frames <- c(all_frames, rep(paste(u_pngDir,"umap_baseplot.png",sep=""), silent_seconds*fps))
-      silent <- silence(duration = silent_seconds, xunit = "time", samp.rate = wav@samp.rate, bit = 16, pcm = TRUE)
-      all_audio <- c(all_audio, silent) 
+      all_frames_umap <- c(all_frames_umap, rep(paste(u_pngDir,"umap_baseplot.png",sep=""), silent_seconds*fps))
+      all_frames_pca <- c(all_frames_pca, rep(paste(p_pngDir,"umap_baseplot.png",sep=""), silent_seconds*fps))
+      if (l==1){
+        silent <- silence(duration = silent_seconds, xunit = "time", samp.rate = wav@samp.rate, bit = 16, pcm = TRUE)
+        all_audio <- c(all_audio, silent) 
+      }
     }
+    
+    u_plot_file <- paste(u_pngDir,"voc",i,"_u_plot.png",sep="")
+    p_plot_file <- paste(p_pngDir,"voc",i,"_p_plot.png",sep="")
     
     # get the number of frames the plot should show, based on the clip duration
     # and pad the audio with a little silence if needed to match the frame res
     audio_dur <- length(wav@left) / wav@samp.rate
     audio_frames <- audio_dur*fps
     n_frames <- ceiling(audio_frames)
-    all_frames <- c(all_frames, rep(u_plot_file, n_frames))
-    pad_dur <- (ceiling(audio_frames)-audio_frames)/fps
-    if (pad_dur > 0){
-      pad <- silence(duration = pad_dur, xunit = "time", samp.rate = wav@samp.rate, bit = 16, pcm = TRUE)
-      all_audio <- c(all_audio, wav, pad)
-    } else{
-      all_audio <- c(all_audio, wav)
+    all_frames_umap <- c(all_frames_umap, rep(u_plot_file, n_frames))
+    all_frames_pca <- c(all_frames_pca, rep(p_plot_file, n_frames))
+    if (l==1){
+      pad_dur <- (ceiling(audio_frames)-audio_frames)/fps
+      if (pad_dur > 0){
+        pad <- silence(duration = pad_dur, xunit = "time", samp.rate = wav@samp.rate, bit = 16, pcm = TRUE)
+        all_audio <- c(all_audio, wav, pad)
+      } else{
+        all_audio <- c(all_audio, wav)
+      } 
     }
     
     # set the previous end time to the end of the current vocalization, setting up for the next voc
     prev_end <- b_u_data$endtime[i]
+    
+    if (l==1){
+      combined_audio <- do.call(bind, all_audio)
+      combined_audio_mono <- mono(combined_audio,"left")
+    }
+    
+  }
+    
+  if (l==1){
+    writeWave(combined_audio_mono, comb_aud_file) 
   }
   
-  combined_audio <- do.call(bind, all_audio)
-  combined_audio_mono <- mono(combined_audio,"left")
-  comb_aud_fp <- paste(umapDir,baby,"_w2v2_layer",l,"_umap.wav",sep="")
-  writeWave(combined_audio_mono, comb_aud_fp)
-  
   av_encode_video(
-    input = all_frames,
+    input = all_frames_umap,
     output = paste(umapDir,baby,"_w2v2_layer_",l,"umap.mp4",sep=""),
     framerate = fps,
-    audio = comb_aud_fp
+    audio = comb_aud_file
+  )
+  av_encode_video(
+    input = all_frames_pca,
+    output = paste(umapDir,baby,"_w2v2_layer_",l,"pca.mp4",sep=""),
+    framerate = fps,
+    audio = comb_aud_file
   )
   
 }
